f06baf3d2f88ad7b22459a2fd4a079b98786a6e7
Rawan Sherif <168760595+rawansheriff@users.noreply.github.com>
2024-09-23 01:43:24 +0400

    Update CASES _Intermediate_SQL.sql

--- CASES _Intermediate_SQL.sql
+++ CASES _Intermediate_SQL.sql
@@ -11,16 +11,16 @@ FROM EmployeeDemographics
 WHERE Age IS NOT NULL
 ORDER BY Age
 
---- THE CASE STATEMENTS IS LIKEMAN IF FUNCTION, YOU SET CONDITIONS TO BE MET ACCORDINGLY
+--- THE CASE STATEMENTS IS LIKE AN IF FUNCTION, YOU SET CONDITIONS TO BE MET ACCORDINGLY  
 --- AND THE FIRST CONDITION IS ALWAYS MET FIRST REGARDLESS IF THE SECOND CONDITION HAS THE SAME CONDITION WITH A DIFFERENT OUTCOME
---- THE OUTCOME OF THE FIRST CONDITION WILL BE SET FIRST 
+--- THE OUTCOME OF THE FIRST CONDITION WILL BE SET/MET FIRST 
  
  SELECT FirstName, LastName, Age, 
  CASE 
     WHEN Age > 30 THEN 'Old'
-	WHEN Age BETWEEN 27 AND 30 THEN 'Young'
-	ELSE 'Baby'
-END 
+    WHEN Age BETWEEN 27 AND 30 THEN 'Young'
+    ELSE 'Baby'
+ END 
  FROM EmployeeDemographics
  WHERE Age IS NOT NULL 
  ORDER BY Age
@@ -30,8 +30,8 @@ END
  CASE 
     WHEN Age = 38 THEN 'Stanley'
     WHEN Age > 30 THEN 'Old'
-	ELSE 'Baby'
-END 
+    ELSE 'Baby'
+ END 
  FROM EmployeeDemographics
  WHERE Age IS NOT NULL 
  ORDER BY Age
@@ -40,11 +40,11 @@ END
  SELECT FirstName, LastName, JobTitle, Salary,
  CASE
     WHEN JobTitle = 'Salesman' THEN Salary + (Salary * .10)
-	WHEN JobTitle = 'Accountant' THEN Salary + (Salary * .05)
-	WHEN JobTitle = 'HR' THEN Salary + (Salary * .000001)
-	ELSE Salary + (Salary * .03)
+    WHEN JobTitle = 'Accountant' THEN Salary + (Salary * .05)
+    WHEN JobTitle = 'HR' THEN Salary + (Salary * .000001)
+    ELSE Salary + (Salary * .03)
 END AS SalaryAfterRaise
  FROM EmployeeDemographics
  JOIN EmployeeSalary
- ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
+  ON EmployeeDemographics.EmployeeID = EmployeeSalary.EmployeeID
 
