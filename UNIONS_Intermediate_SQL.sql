934df2a4edb7cbd18bb911dca59bb1c749593f42
Rawan Sherif <168760595+rawansheriff@users.noreply.github.com>
2024-09-23 01:36:40 +0400

    Update UNIONS_Intermediate_SQL.sql

--- UNIONS_Intermediate_SQL.sql
+++ UNIONS_Intermediate_SQL.sql
@@ -1,18 +1,18 @@
---CREATE TABLE WareHouseEmployeeDemographics 
---( EmployeeID int,
---FirstName varchar (50),
---LastName varchar (50),
---Age int,
---Gender varchar (50))
+CREATE TABLE WareHouseEmployeeDemographics 
+( EmployeeID int,
+FirstName varchar (50),
+LastName varchar (50),
+Age int,
+Gender varchar (50))
 
---SELECT *
---FROM WareHouseEmployeeDemographics;
+SELECT *
+FROM WareHouseEmployeeDemographics;
 
---INSERT INTO WareHouseEmployeeDemographics VALUES
---(1013,'Darryl','Philbin',NULL,'Male'),
---(1050,'Roy', 'Anderson', 31,'Male'),
---(1051,'Hidetoshi','Hasagawa', 40,'Male'),
---(1052,'Val', 'Johnson',31,'Female')
+INSERT INTO WareHouseEmployeeDemographics VALUES
+(1013,'Darryl','Philbin',NULL,'Male'),
+1050,'Roy', 'Anderson', 31,'Male'),
+(1051,'Hidetoshi','Hasagawa', 40,'Male'),
+(1052,'Val', 'Johnson',31,'Female')
 
 SELECT *
 FROM EmployeeDemographics
@@ -35,7 +35,7 @@ SELECT *
 FROM WareHouseEmployeeDemographics
 ORDER BY EmployeeID
 
---- EVEN WHEN THE TABLES DONT HAVE THE SAME NUMBERS OF COLUMNS YOU CAN CHOOSE WHICH FIELDS TO SELECT 
+--- EVEN WHEN THE TABLES DON'T HAVE THE SAME NUMBERS OF COLUMNS YOU CAN CHOOSE WHICH FIELDS TO SELECT 
 
 SELECT EmployeeID, FirstName, Age
 FROM EmployeeDemographics
